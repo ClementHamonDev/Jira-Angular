dcb7bd8ec2163ff3e54c5636e86ab05735876232
ClementHamonDev <clement.hamon35@gmail.com>
2025-02-27 14:27:46 +0100

    tp 2 3 4

--- Jira/src/app/list/list.component.ts
+++ Jira/src/app/list/list.component.ts
@@ -1,14 +1,44 @@
-import { Component, Input } from '@angular/core';
+import { Component, effect, Input, signal } from '@angular/core';
 import { TaskComponent } from "../task/task.component";
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-list',
-  imports: [TaskComponent, CommonModule],
+  imports: [TaskComponent, CommonModule, ReactiveFormsModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
   @Input() list: { title: string; tasks: { title: string; description: string }[] } = { title: '', tasks: [] };  // On reçoit une liste de tâches
+  listSignal = signal(this.list);
+
+  ngOnInit() {
+    effect(() => {
+      alert('La liste a été modifiée: ' + this.listSignal());
+    });
+  }
+
+  taskForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.taskForm = this.fb.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+  }
+
+  addTask() {
+    if (this.taskForm.valid) {
+      this.list.tasks.push(this.taskForm.value);
+      this.listSignal.set(this.list);
+      this.taskForm.reset();
+    }
+  }
+
+  deleteTask(title: string) {
+    this.list.tasks = this.list.tasks.filter(task => task.title !== title);
+    this.listSignal.set(this.list);
+  }
 
 }
